1a87ac27519fc9fd8ecb20d1fbe6d5408598495e
Manuel E. Bencosme C <manueb149@gmail.com>
2020-04-01 18:02:09 -0400

    first commit

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React, { Fragment } from "react";
+
+function App() {
+  return (
+    <Fragment>
+      <h1>Administrador de Pacientes</h1>
+      
+      <div className="container">
+
+      </div>
+    </Fragment>
+  );
+}
+
+export default App;
